e2cdfd5404e46d97b18525b00b0238424e4d0065
Samantha Haldeman <samkh@colostate.edu>
2025-04-07 10:10:28 -0600

    about info

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TeamMemberComponent } from '../team-member/team-member.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [TeamMemberComponent],
   standalone: true,
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  SKHimagePath = 'assets/images/SKHpic.jpg';
 }
